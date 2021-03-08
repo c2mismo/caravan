476917d687d08a4befa909b67dab2fc9b58ddfba
c2mismo <c2mismo@tuta.io>
2021-03-08 15:53:06 +0100

    Add lastCHN from canbus

--- general_relays/src/main.cpp
+++ general_relays/src/main.cpp
@@ -15,31 +15,36 @@ struct can_frame canMsg12_230;
 MCP2515 mcp2515(53);
 
 //  Read canBus 230box Values
-byte val230Left;
-byte val230Right;
+byte val230Home;
 byte val230In;
+byte val230Right;
+byte val230Left;
 byte val230Amper;
 
 //  Read canBus 230box States
-bool state230Automatic;
-bool state230LeftIn;
-bool state230LeftOut;
-bool state230RightIn;
-bool state230RightOut;
-bool state230CHR;
-bool state230Home;
-bool state230Inverter;
+byte state230Automatic;
+byte state230LeftIn;
+byte state230LeftOut;
+byte state230RightIn;
+byte state230RightOut;
+byte state230CHR;
+byte state230Home;
+byte state230Inverter;
 
 //  Send canBus 230box Signals
-bool signal230Left;
-bool signal230Right;
+bool testSignal230In;
+bool testSignal230Home;
+bool testSignal230Right;
+bool tsetSignal230Left;
 
 //         END LIBRARY CONF
 
 //             PINS
 
-const int pinSignal230Left = 7;
-const int pinSignal230Right = 8;
+const int pinTestSignal230In = 8;
+const int pinTestSignal230Home = 9;
+const int pinTestSignal230Left = 10; // Simul 230 Volt for test
+const int pinTestSignal230Right = 11;
 
 const int ledVerde = 2;
 const int reles16_1 = A0;
@@ -52,6 +57,7 @@ const int reles16_1 = A0;
 //         END VARIABLES
 
 void setup(){
+  while (!Serial);
   Serial.begin(115200);
   SPI.begin();
 
@@ -59,10 +65,12 @@ void setup(){
   mcp2515.setBitrate (CAN_500KBPS, MCP_8MHZ);
   mcp2515.setNormalMode();
   canMsg12_230.can_id = 0x038;
-  canMsg12_230.can_dlc = 2;
+  canMsg12_230.can_dlc = 4;
 
-  pinMode(pinSignal230Left, INPUT_PULLUP);
-  pinMode(pinSignal230Right, INPUT_PULLUP);
+  pinMode(pinTestSignal230In, INPUT_PULLUP);
+  pinMode(pinTestSignal230Home, INPUT_PULLUP);
+  pinMode(pinTestSignal230Left, INPUT_PULLUP);
+  pinMode(pinTestSignal230Right, INPUT_PULLUP);
 
   pinMode(ledVerde, OUTPUT);
   pinMode(reles16_1, OUTPUT);
@@ -72,23 +80,32 @@ void loop(){
 
 
   // Rear data from 230box Values
+  val230Home=0;val230In=0;val230Right=0;val230Left=0;val230Amper=0;
   if (mcp2515.readMessage(&canMsgValue230_12) == MCP2515::ERROR_OK)
   {
-    val230Left = canMsgValue230_12.data[0]; // >=255 ERROR
-    val230Right = canMsgValue230_12.data[1];
-    val230In = canMsgValue230_12.data[2];
-    val230Amper = canMsgValue230_12.data[3];
+    val230Home = canMsgValue230_12.data[0];
+    val230In = canMsgValue230_12.data[1];
+    val230Right = canMsgValue230_12.data[2];
+    val230Left = canMsgValue230_12.data[3]; // >=255 ERROR
+    val230Amper = canMsgValue230_12.data[4];
   }
-  Serial.print(" val230Left: ");
-  Serial.println(val230Left);
-  Serial.print(" val230Right: ");
-  Serial.println(val230Right);
+
+  Serial.print(" val230Home: ");
+  Serial.println(val230Home);
   Serial.print(" val230In: ");
   Serial.println(val230In);
+  Serial.print(" val230Right: ");
+  Serial.println(val230Right);
+  Serial.print(" val230Left: ");
+  Serial.println(val230Left);
+  Serial.print(" val230Amper: ");
+  Serial.println(val230Amper);
   Serial.println("***************");
   // END rear data from 230box Values
 
   // Rear data from 230box States
+  // Poner valores a CERO
+  state230Automatic=0;state230LeftIn=0;state230LeftOut=0;state230RightIn=0;state230RightOut=0;state230CHR=0;state230Home=0;state230Inverter=0;
   if (mcp2515.readMessage(&canMsgState230_12) == MCP2515::ERROR_OK)
   {
     state230Automatic = canMsgState230_12.data[0];
@@ -100,31 +117,43 @@ void loop(){
     state230Home = canMsgState230_12.data[6];
     state230Inverter = canMsgState230_12.data[7];
   }
+
+  Serial.print(" state230Automatic: ");
+  Serial.println(state230Automatic);
+  Serial.print(" state230LeftIn: ");
+  Serial.println(state230LeftIn);
+  Serial.print(" state230LeftOut: ");
+  Serial.println(state230LeftOut);
+  Serial.print(" state230RightIn: ");
+  Serial.println(state230RightIn);
+  Serial.print(" state230RightOut: ");
+  Serial.println(state230RightOut);
+  Serial.print(" state230CHR: ");
+  Serial.println(state230CHR);
+  Serial.print(" state230Home: ");
+  Serial.println(state230Home);
+  Serial.print(" state230Inverter: ");
+  Serial.println(state230Inverter);
+  Serial.println("***************");
   // END rear data from 230box States
 
   //  Send data to 230box
-  if (digitalRead(pinSignal230Left))
-  {
-    digitalWrite(ledVerde, LOW);
-    signal230Left = false;
-    canMsg12_230.data[0] = signal230Left;
-  } else {
-    digitalWrite(ledVerde, HIGH);
-    signal230Left = true;
-    canMsg12_230.data[0] = signal230Left;
-  }
-  if (digitalRead(pinSignal230Right))
-  {
-    digitalWrite(reles16_1, HIGH);
-    signal230Right = false;
-    canMsg12_230.data[1] = signal230Right;
-  } else {
-    digitalWrite(reles16_1, LOW);
-    signal230Right = true;
-    canMsg12_230.data[1] = signal230Right;
-  }
+  canMsg12_230.data[0] = digitalRead(pinTestSignal230In);
+  canMsg12_230.data[1] = digitalRead(pinTestSignal230Home);
+  canMsg12_230.data[2] = digitalRead(pinTestSignal230Right);
+  canMsg12_230.data[3] = digitalRead(pinTestSignal230Left);
   mcp2515.sendMessage(&canMsg12_230);
   // END send data to 230box
+
+
+  if (state230Inverter)
+  {
+    digitalWrite(ledVerde, HIGH);
+  }
+  if (!state230Inverter)
+  {
+    digitalWrite(ledVerde, LOW);
+  }
 
   delay(1500);
 }
